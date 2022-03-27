20b472b5e71848e33eda3f2a172af8c0a43aabc7
Dark <istiak.csinfo@gmail.com>
2022-03-27 23:40:55 +0600

    dom+module

--- index.js
+++ index.js
@@ -14,23 +14,28 @@
 
 
 */
-foundMonster()
+import { monsters } from './monsters.js'
+// console.log(monsters)
+for (let mnstr of monsters) {
+    foundMonster(mnstr)
+}
 
-function foundMonster() {
+
+function foundMonster(mnstr) {
     const monster = document.createElement('div')
     monster.className = 'monster'
 
     const img = document.createElement('img')
-    img.src = "https://robohash.org/10?set=set2";
-    img.alt = 'Istiak Ahmed'
+    img.src = `https://robohash.org/${mnstr.id}?set=set2`;
+    img.alt = mnstr.name
 
     const names = document.createElement('p')
     names.className = 'name';
-    names.innerText = 'Istiak Ahmed'
+    names.innerText = mnstr.name
 
     const email = document.createElement('p')
     email.className = 'email'
-    email.innerText = 'istiak.csinfo@gmail.com';
+    email.innerText = mnstr.email;
 
     monster.append(img, names, email);
     document.querySelector('.monsters').append(monster);
@@ -46,6 +51,7 @@ notMatch();
 function notMatch() {
     const notmatched = document.createElement('div')
     notmatched.className = 'p-5 not-found'
+    notmatched.id = 'not-found'
     notmatched.style = "display: none"
 
     const span = document.createElement("span");
@@ -60,3 +66,39 @@ function notMatch() {
     // console.log(notmatched)
 
 }
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>Filtering
+
+document.querySelector('.search__field')
+    .addEventListener('keyup', function(e) {
+        const keyword = e.target.value.toLowerCase();
+        // console.log(e);
+        console.log(keyword);
+        let notfound = true;
+        const mnstr = document.querySelectorAll('.monster');
+        for (let monster of mnstr) {
+            const name = monster.children[1].innerText.toLowerCase();
+            const email = monster.children[2].innerText.toLowerCase();
+
+            if (name.includes(keyword) || email.includes(keyword)) {
+                // console.log('done');
+                monster.style.display = 'block';
+                notfound = false;
+            } else {
+                monster.style.display = 'none';
+            }
+        }
+        if (notfound) {
+            document.querySelector('#not-found').style.display = 'block'
+        } else {
+            document.querySelector('#not-found').style.display = 'none'
+        }
+
+    })
+
+
+// deactive search refresh
+document.querySelector('#search-monster-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+})
